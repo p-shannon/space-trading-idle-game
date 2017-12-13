fa1416db1f3145c16b3486a7bd31f04c676295ee
Patrick Shannon <patrickshannon05021993@gmail.com>
2017-12-13 13:42:24 -0500

    Adds login, and register support to the users controller.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApiController
+	before_action :require_login, except: [:create]
+
+	def create
+		user = User.create!(user_params)
+		render json: { token: user.auth_token }
+	end
+
+	def profile user = User.find_by_auth_token!(request.headers[:token])
+		render json {user: {username: user.username, data: user.data}}
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:username, :password)
+	end
+
+end
